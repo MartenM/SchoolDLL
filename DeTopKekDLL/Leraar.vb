3eeb77d1ff252b62af2013161e8d7b0572fd393f
Student <Student@STATION-525>
2017-10-11 09:53:18 +0200

    Fix Leraar Class

--- DeTopKekDLL/Leraar.vb
+++ DeTopKekDLL/Leraar.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Leraar
 
+    Inherits Persoon
+
     Private _vak As String 'Idk what else
     Public Property vak() As String
         Get
@@ -9,4 +11,20 @@
             _vak = value
         End Set
     End Property
+
+    Private _mentorklas As String = ""
+    Public ReadOnly Property hasMentorKlas As Boolean
+        Get
+            Return _mentorklas IsNot ""
+        End Get
+    End Property
+    Public Property mentorKlas As String
+        Get
+            Return _mentorklas
+        End Get
+        Set(value As String)
+            _mentorklas = value
+        End Set
+    End Property
+
 End Class
